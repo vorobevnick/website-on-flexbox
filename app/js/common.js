96c6e8a62c4e4de075c0f9bb158610a5e8094dac
vorobevnick <sparinf@mail.ru>
2018-05-18 00:14:49 +0500

    adaptive section partners and footer was added, small refactoring

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,46 @@
 $(function() {
-    $('.slider').slick({
+    $('.slider').slick();
 
+    $('.slider-partners').slick({
+        infinite: true,
+        slidesToShow: 5,
+        centerMode: true,
+        centerPadding: '15px',
+
+        responsive: [
+        {
+            breakpoint: 1200,
+            settings: {
+                slidesToShow: 3,
+                infinite: true,
+                centerPadding: '10px'
+            }
+        },
+        {
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 2,
+                centerMode: true,
+                centerPadding: '0'
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1,
+                centerMode: true,
+                centerPadding: '0'
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                centerMode: true,
+                centerPadding: '0'
+            }
+         }
+        ]
     });
+
 });
